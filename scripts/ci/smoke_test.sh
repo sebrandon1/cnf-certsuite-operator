0bd03ad59499a7d54e29588e2a94a38fd9782279
Gonzalo Reyero Ferreras <87083379+greyerof@users.noreply.github.com>
2025-01-07 10:27:07 +0100

    Adjust expected results for smoke test. (#167)

--- scripts/ci/smoke_test.sh
+++ scripts/ci/smoke_test.sh
@@ -60,10 +60,10 @@ echo "$crJson" | jq
 
 # Run checks for verdict and counters.
 export EXPECTED_VERDICT=${EXPECTED_VERDICT:-"pass"}
-export EXPECTED_TOTAL_TCS=${EXPECTED_TOTAL_TCS:-"100"}
+export EXPECTED_TOTAL_TCS=${EXPECTED_TOTAL_TCS:-"99"}
 export EXPECTED_FAILED=${EXPECTED_FAILED:-"0"}
 export EXPECTED_PASSED=${EXPECTED_PASSED:-"5"}
-export EXPECTED_SKIPPED=${EXPECTED_SKIPPED:-"95"}
+export EXPECTED_SKIPPED=${EXPECTED_SKIPPED:-"94"}
 
 # Check the verdit is pass
 echo "$crJson" | jq 'if .status.report.verdict == env.EXPECTED_VERDICT then "verdict is "+env.EXPECTED_VERDICT else error("verdict mismatch: \(.status.report.verdict), expected "+env.EXPECTED_VERDICT) end'
