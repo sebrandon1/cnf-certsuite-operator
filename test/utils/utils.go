c31308fe5d69e6c5235c19e651fa1d77813cf7d5
Brandon Palm <bpalm@redhat.com>
2025-04-24 13:27:14 -0500

    Update cert-manager to v1.17.2

--- test/utils/utils.go
+++ test/utils/utils.go
@@ -30,7 +30,7 @@ const (
 	prometheusOperatorURL     = "https://github.com/prometheus-operator/prometheus-operator/" +
 		"releases/download/%s/bundle.yaml"
 
-	certmanagerVersion = "v1.5.3"
+	certmanagerVersion = "v1.17.2"
 	certmanagerURLTmpl = "https://github.com/jetstack/cert-manager/releases/download/%s/cert-manager.yaml"
 )
 
